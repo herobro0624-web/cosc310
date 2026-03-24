7e4580ff0a9578e5d1aed1026f157421c8175f64
Aiden Benton <herobro0624@gmail.com>
2026-03-24 11:15:11 -0500

    Sorting Work

--- chapter14/Sorting.java
+++ chapter14/Sorting.java
@@ -4,26 +4,6 @@ import java.util.ArrayList;
 
 public class Sorting {
 
-<<<<<<< HEAD
-    private static void swap(ArrayList<Comparable> list, int i, int j) {
-        Comparable temp = list.get(i);
-        list.set(i, list.get(j));
-        list.set(j, temp);
-    }
-
-    // Comparable
-    // obj1.compareTo(obj2) ... returns -1 if obj1 is "less than" obj2
-    // obj1.compareTo(obj2) ... returns 0 if obj1 is "equal to" obj2
-    // obj1.compareTo(obj2) ... returns 1 if obj1 is "greater than" obj2
-    // this is a destructive method, i.e. it modifies the original list
-    public static void selectionSort(ArrayList<Comparable> list) {
-        for (int i = 0; i < list.size() - 1; i++) {
-            Comparable smallest = list.get(i + 1);
-            int smallesti = i;
-            for (int j = i + 1; j < list.size(); j++) {
-                Comparable c = list.get(j);
-                if (c.compareTo(smallest) < 0){
-=======
     private static <T extends Comparable<T>> void swap(ArrayList<T> list, int i, int j) {
         T tmp = list.get(i);
         list.set(i, list.get(j));
@@ -47,23 +27,12 @@ public class Sorting {
             for (int j=i+1; j<list.size(); j++) {
                 T c = list.get(j);
                 if (c.compareTo(smallest)<0) {
->>>>>>> 3077e7ce941195c7cec1014b38ca993d34373538
                     smallest = c;
                     smallesti = j;
                 }
             }
             swap(list, i, smallesti);
         }
-<<<<<<< HEAD
-
-        
-    }
-    public static void main(String[] args) {
-        ArrayList<Integer> test = new ArrayList<>();
-        test.add(null);
-    }
-    
-=======
     }
 
     public static <T extends Comparable<T>> void insertionSort(ArrayList<T> list) {
@@ -95,5 +64,4 @@ public class Sorting {
         System.out.println(test);
     }
 
->>>>>>> 3077e7ce941195c7cec1014b38ca993d34373538
 }
