d359130222ab58dd526e87d35f3a56bed07abeac
Aiden Benton <herobro0624@gmail.com>
2026-03-31 11:05:04 -0500

    Did a couple with Lab3

--- chapter14/Lab3.java
+++ chapter14/Lab3.java
@@ -7,9 +7,9 @@ import json.*;
 
 public class Lab3 {
     public static void main(String[] args) throws Exception {
-        ArrayList<BikeDataRecord> records = BikeDataReader.parse("json/day2.json");
+        ArrayList<BikeDataRecord> records = BikeDataReader.parse("json/day1.json");
         System.out.println(records.size());
-        System.out.println(records.get(20));
+        System.out.println(records.get(0));
         List<BikeDataRecord> sortedRecords = Sorting.mergeSort(records);
         System.out.println(records.get(0));
 
